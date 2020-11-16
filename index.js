0d0cd139a5a84ceb9ca55019b91e6ebd912c5ce3
Brandon Mikeska <mikeska.brandon@corp.sysco.com>
2020-11-16 15:59:49 -0600

    Update index.js
    Remove unnessary encoding of the url string.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const core = require('@actions/core');
-const exec = require('child_process').exec;
+const core = require("@actions/core");
+const exec = require("child_process").exec;
 
 try {
   generateLogin();
@@ -8,11 +8,14 @@ try {
 }
 
 function generateLogin() {
-  exec('sfdx force:user:password:generate', function (error, stdout, stderr) {
+  exec("sfdx force:user:password:generate", function (error, stdout, stderr) {
     if (error) console.log(stderr);
-    exec('sfdx force:user:display --json', function (error1, stdout1, stderr1) {
+    exec("sfdx force:user:display --json", function (error1, stdout1, stderr1) {
       const { result } = JSON.parse(stdout1);
-      core.setOutput('login-url', encodeURI(`${result.loginUrl}/?un=${result.username}&pw=${result.password}`));
+      core.setOutput(
+        "login-url",
+        `${result.loginUrl}/?un=${result.username}&pw=${result.password}`
+      );
     });
   });
 }
